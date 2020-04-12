ec77289605ee08aa3828a8a8be8a187105256ac9
Julian Nadeau <jules2689@github.com>
2020-04-11 20:08:01 -0400

    Dynamic app and source menus

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,17 +1,16 @@
 class LogsController < ApplicationController
   include QueryParser
+  before_action :set_aggregations, only: :index
+
   def index
     search_params = parse_query(params)
-    puts JSON.pretty_generate(search_params)
+    puts JSON.pretty_generate(search_params) if ENV['FULL_LOGGING']
     @logs = LOG_REPO.search(search_params)
-  end
-
-  def new
   end
 
   def create
     original_log = request.body.read
-    puts original_log
+    puts original_log if ENV['FULL_LOGGING']
 
     # Parse the log
     log = original_log.gsub(/\e\]0;/, '')
@@ -20,6 +19,7 @@ class LogsController < ApplicationController
     preamble, log = log.include?("\e[1m") ? log.split("\e[1m", 2) : log.split("\e[0m | ", 2)
     source, _ = Strings::ANSI.sanitize(preamble).split(" | ")
   
+    # Save the log
     if source != "echo" && log&.strip&.present?
       tags = Strings::ANSI.sanitize(log).scan(/\[(\w+)\]/).map(&:first)
       new_log = GenericLog.new(
@@ -32,5 +32,22 @@ class LogsController < ApplicationController
     end
 
     head :no_content
+  end
+
+  private
+
+  def set_aggregations
+    @aggregations ||= begin
+      query = {
+        size: 0,
+        aggs: {
+          applications: { terms: { field: "application",  size: 500 } },
+          sources: { terms: { field: "source",  size: 500 } }
+        }
+      }
+      response = LOG_REPO.search(query)
+      aggregations = response.raw_response["aggregations"]
+      aggregations.map { |k, v| [k, v["buckets"].map { |b| b["key"] }.sort] }.to_h
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module Timber
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    unless ENV['FULL_LOGGING']
+      config.lograge.enabled = true
+      config.lograge.custom_payload do |controller|
+        if controller.params[:application]
+          { app: controller.params[:application] }
+        end
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
